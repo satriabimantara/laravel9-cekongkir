1a75e5a0ec3e5e7c92189e3b05540d9cb66fb3ae
I Made Satria Bimantara <satriabimantara.md@gmail.com>
2024-05-31 14:06:58 +0800

    - Modifying webpack.mix.js structure to call multiple js files

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,12 +10,6 @@ const mix = require("laravel-mix");
  | file for the application as well as bundling up all the JS files.
  |
  */
-mix.js("resources/js/app.js", "public/js").postCss(
-    "resources/css/styles.css",
-    "public/css",
-    [
-        //
-    ]
-);
-mix.js("resources/js/ongkir.js", "public/js");
-mix.sass("resources/sass/app.scss", "public/css");
+mix.js(["resources/js/app.js", "resources/js/ongkir.js"], "public/js")
+    .postCss("resources/css/styles.css", "public/css", [])
+    .sass("resources/sass/app.scss", "public/css");
